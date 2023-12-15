e81ebc08eaa34449614b219f8d7f8c3f5568a8b6
Eugene <eugene@klio.dp.ua>
2023-12-15 20:59:34 +0200

    Parameters

--- KlioBlazor/wwwroot/js/main.js
+++ KlioBlazor/wwwroot/js/main.js
@@ -2,37 +2,21 @@
 	"use strict";
 
 	$(window).on('load', function () {
-		console.log("loaded");
-	});
-
-	/*=============================================
-	=    		Mobile Menu			              =
-    =============================================*/
-	//SubMenu Dropdown Toggle
-	if ($('.menu-area li.menu-item-has-children ul').length) {
-		$('.menu-area .navigation li.menu-item-has-children').append('<div class="dropdown-btn"><span class="fa-solid fa-angle-down"></span></div>');
-	}
-	//Mobile Nav Hide Show
-	if ($('.mobile-menu').length) {
-
-		var mobileMenuContent = $('.menu-area .main-menu').html();
-		$('.mobile-menu .menu-box .menu-outer').append(mobileMenuContent);
-
-		//Dropdown Button
 		$('.mobile-menu li.menu-item-has-children .dropdown-btn').on('click', function () {
 			$(this).toggleClass('open');
 			$(this).prev('ul').slideToggle(500);
 		});
-		//Menu Toggle Btn
+
 		$('.mobile-nav-toggler').on('click', function () {
 			$('body').addClass('mobile-menu-visible');
 		});
 
-		//Menu Toggle Btn
 		$('.menu-backdrop, .mobile-menu .close-btn').on('click', function () {
 			$('body').removeClass('mobile-menu-visible');
 		});
-	}
+
+		console.log("loaded");
+	});
 
 	/*=============================================
 	=     Menu sticky & Scroll to top			  =
