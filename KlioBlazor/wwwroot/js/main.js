9346dd0d9a680047a8bc55fa06139618267bb034
Eugene <eugene@klio.dp.ua>
2023-12-15 17:05:16 +0200

    Basic design

---
+++ KlioBlazor/wwwroot/js/main.js
@@ -0,0 +1,65 @@
+﻿(function ($) {
+	"use strict";
+
+	$(window).on('load', function () {
+		console.log("loaded");
+	});
+
+	/*=============================================
+	=    		Mobile Menu			              =
+    =============================================*/
+	//SubMenu Dropdown Toggle
+	if ($('.menu-area li.menu-item-has-children ul').length) {
+		$('.menu-area .navigation li.menu-item-has-children').append('<div class="dropdown-btn"><span class="fa-solid fa-angle-down"></span></div>');
+	}
+	//Mobile Nav Hide Show
+	if ($('.mobile-menu').length) {
+
+		var mobileMenuContent = $('.menu-area .main-menu').html();
+		$('.mobile-menu .menu-box .menu-outer').append(mobileMenuContent);
+
+		//Dropdown Button
+		$('.mobile-menu li.menu-item-has-children .dropdown-btn').on('click', function () {
+			$(this).toggleClass('open');
+			$(this).prev('ul').slideToggle(500);
+		});
+		//Menu Toggle Btn
+		$('.mobile-nav-toggler').on('click', function () {
+			$('body').addClass('mobile-menu-visible');
+		});
+
+		//Menu Toggle Btn
+		$('.menu-backdrop, .mobile-menu .close-btn').on('click', function () {
+			$('body').removeClass('mobile-menu-visible');
+		});
+	}
+
+	/*=============================================
+	=     Menu sticky & Scroll to top			  =
+	=============================================*/
+	$(window).on('scroll', function () {
+		var scroll = $(window).scrollTop();
+		if (scroll < 245) {
+			$("#sticky-header").removeClass("sticky-menu");
+			$('.scroll-to-target').removeClass('open');
+
+		} else {
+			$("#sticky-header").addClass("sticky-menu");
+			$('.scroll-to-target').addClass('open');
+		}
+	});
+
+	/*=============================================
+	=    		 Scroll Up  	                  =
+	=============================================*/
+	if ($('.scroll-to-target').length) {
+		$(".scroll-to-target").on('click', function () {
+			var target = $(this).attr('data-target');
+			// animate
+			$('html, body').animate({
+				scrollTop: $(target).offset().top
+			}, 1000);
+
+		});
+	}
+})(jQuery);
