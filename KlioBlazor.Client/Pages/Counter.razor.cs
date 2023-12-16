ad1c308b5d48abedf085649cc23018b221ddfc82
Eugene <eugene@klio.dp.ua>
2023-12-16 18:43:37 +0200

    Partial Classes

---
+++ KlioBlazor.Client/Pages/Counter.razor.cs
@@ -0,0 +1,12 @@
+﻿namespace KlioBlazor.Client.Pages
+{
+    public partial class Counter
+    {
+        private int currentCount = 0;
+
+        private void IncrementCount()
+        {
+            currentCount++;
+        }
+    }
+}
