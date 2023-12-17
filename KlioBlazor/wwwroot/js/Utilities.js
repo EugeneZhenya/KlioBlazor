9255f8b63c22c30fffa6b2e916b719b9d7458796
Eugene <eugene@klio.dp.ua>
2023-12-17 11:02:57 +0200

    Invoking Static C# Methods from JavaScript

--- KlioBlazor/wwwroot/js/Utilities.js
+++ KlioBlazor/wwwroot/js/Utilities.js
@@ -1,3 +1,10 @@
 ﻿function my_function(message) {
     console.log("from utilities " + message);
 }
+
+function dotnetStaticInvocation() {
+    DotNet.invokeMethodAsync("KlioBlazor", "GetCurrentCount")
+        .then(result => {
+            console.log("count from javascript " + result);
+        });
+}
