9cc7a7a8106561dfba5d2ab0f51f72270bcb664f
Eugene <eugene@klio.dp.ua>
2023-12-22 21:27:00 +0200

    Route Params

--- KlioBlazor.Shared/Entities/Movie.cs
+++ KlioBlazor.Shared/Entities/Movie.cs
@@ -2,6 +2,7 @@
 {
     public class Movie
     {
+        public int Id { get; set; } = 1;
         public string Title { get; set; }
         public DateTime ReleaseDate { get; set; }
         public string Poster { get; set; }
--- KlioBlazor/Helpers/StringUtilities.cs
+++ KlioBlazor/Helpers/StringUtilities.cs
@@ -1,4 +1,5 @@
-﻿using System.Text.RegularExpressions;
+﻿using System.Text;
+using System.Text.RegularExpressions;
 
 namespace KlioBlazor.Helpers
 {
@@ -35,6 +36,23 @@ namespace KlioBlazor.Helpers
                 words = list.ToArray();
             }
             return words;
+        }
+
+        public static string Translit(string source)
+        {
+            StringBuilder ret = new StringBuilder();
+            string[] cyr = { " ", "+", "-", "1", "2", "3", "4", "5", "6", "7", "8", "9", "0", "A", "a", "B", "b", "C", "c", "D", "d", "E", "e", "F", "f", "G", "g", "H", "h", "I", "i", "J", "j", "K", "k", "L", "l", "M", "m", "N", "n", "O", "o", "P", "p", "Q", "q", "R", "r", "S", "s", "T", "t", "U", "u", "V", "v", "W", "w", "X", "x", "Y", "y", "Z", "z", "А", "а", "Б", "б", "В", "в", "Г", "г", "Ґ", "ґ", "Д", "д", "Е", "е", "Є" , "є" , "Ж" , "ж" , "З", "з", "И", "и", "І", "і", "Ї" , "ї", "Й", "й", "К", "к", "Л", "л", "М", "м", "Н", "н", "О", "о", "П", "п", "Р", "р", "С", "с", "Т", "т", "У", "у", "Ф", "ф", "Х" , "х" , "Ц" , "ц" , "Ч" , "ч" , "Ш" , "ш" , "Щ"   , "щ"   , "Ю" , "ю" , "Я" , "я"  };
+            string[] lat = { "_", "+", "-", "1", "2", "3", "4", "5", "6", "7", "8", "9", "0", "A", "a", "B", "b", "C", "c", "D", "d", "E", "e", "F", "f", "G", "g", "H", "h", "I", "i", "J", "j", "K", "k", "L", "l", "M", "m", "N", "n", "O", "o", "P", "p", "Q", "q", "R", "r", "S", "s", "T", "t", "U", "u", "V", "v", "W", "w", "X", "x", "Y", "y", "Z", "z", "A", "a", "B", "b", "V", "v", "H", "h", "G", "g", "D", "d", "E", "e", "Ye", "ie", "Zh", "zh", "Z", "z", "Y", "y", "I", "i", "Yi", "i", "Y", "y", "K", "k", "L", "l", "M", "m", "N", "n", "O", "o", "P", "p", "R", "r", "S", "s", "T", "t", "U", "u", "F", "f", "Kh", "kh", "Ts", "ts", "Ch", "ch", "Sh", "sh", "Shch", "shch", "Yu", "iu", "Ya", "ia" };
+
+            for (int j = 0; j < source.Length; j++)
+            {
+                for (int i = 0; i < cyr.Length; i++)
+                {
+                    if (source.Substring(j, 1) == cyr[i]) ret.Append(lat[i]);
+                }
+            }
+
+            return ret.ToString();
         }
     }
 }
