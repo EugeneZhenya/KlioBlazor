918b63c4244926157380f375916546fcf1898a0e
Eugene <eugene@klio.dp.ua>
2023-12-22 13:30:03 +0200

    App.razor File

---
+++ KlioBlazor.Shared/OpenGraphModel.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace KlioBlazor.Shared
+{
+    public class OpenGraphModel
+    {
+        public string Title { get; set; }
+        public string Description { get; set; }
+        public string Robots { get; set; }
+        public string Keywords { get; set; }
+        public string Type { get; set; }
+        public string Author { get; set; }
+        public string Image { get; set; }
+        public string SecureImage { get; set; }
+        public int ImageWidth { get; set; }
+        public int ImageHeight { get; set; }
+        public string ImageType { get; set; }
+        public string Url { get; set; }
+        public string SiteName { get; set; }
+        public string Locale { get; set; }
+        public string FBAdmins { get; set; }
+        public string FBAppId { get; set; }
+        public string Image2 { get; set; }
+        public string SecureImage2 { get; set; }
+        public int ImageWidth2 { get; set; }
+        public int ImageHeight2 { get; set; }
+        public string ImageType2 { get; set; }
+    }
+}
