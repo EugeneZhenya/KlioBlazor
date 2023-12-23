6739fc4ce8ce71fb16a59fdaea73bb2b094a89d9
Eugene <eugene@klio.dp.ua>
2023-12-23 22:53:43 +0200

    Validations with Data Annotations

--- KlioBlazor.Shared/Entities/Category.cs
+++ KlioBlazor.Shared/Entities/Category.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -9,6 +10,7 @@ namespace KlioBlazor.Shared.Entities
     public class Category
     {
         public int Id { get; set; }
+        [Required(ErrorMessage = "Поле \"Назва\" не може бути порожнім.")]
         public string Name { get; set; }
         public string Description { get; set; }
     }
