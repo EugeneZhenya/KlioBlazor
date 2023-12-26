bfacf8f587fc13eb7110cf224b5d331c0d3ad1f9
Eugene <eugene@klio.dp.ua>
2023-12-26 11:01:19 +0200

    Person Form

---
+++ KlioBlazor.Shared/Entities/Person.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace KlioBlazor.Shared.Entities
+{
+    public class Person
+    {
+        public int Id { get; set; }
+        [Required(ErrorMessage = "Поле \"Ім'я\" не може бути порожнім.")]
+        public string Name { get; set; }
+        public string Biography { get; set; }
+        public string Picture { get; set; }
+        public DateTime? DateOfBirth { get; set; }
+        public bool DateOfBirthExact { get; set; }
+        public DateTime? DateOfDeath { get; set; }
+        public bool DateOfDeathExact { get; set; }
+        public bool IsFemale { get; set; }
+    }
+}
