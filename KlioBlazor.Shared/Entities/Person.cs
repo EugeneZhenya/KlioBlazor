786e7ecb77e758c0f81a97c95da18e38d4b4925d
Eugene <eugene@klio.dp.ua>
2023-12-26 14:50:19 +0200

    Component for Inserting Images

--- KlioBlazor.Shared/Entities/Person.cs
+++ KlioBlazor.Shared/Entities/Person.cs
@@ -13,7 +13,7 @@ namespace KlioBlazor.Shared.Entities
         [Required(ErrorMessage = "Поле \"Ім'я\" не може бути порожнім.")]
         public string Name { get; set; }
         public string Biography { get; set; }
-        public string Picture { get; set; }
+        public string? Picture { get; set; }
         public DateTime? DateOfBirth { get; set; }
         public bool DateOfBirthExact { get; set; }
         public DateTime? DateOfDeath { get; set; }
