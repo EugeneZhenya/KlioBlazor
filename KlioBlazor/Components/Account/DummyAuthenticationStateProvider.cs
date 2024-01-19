a1dd7135183823e2902e0460e1054bb2c4f2075c
Eugene <eugene@klio.dp.ua>
2024-01-19 12:06:43 +0200

    AuthorizeView - Hiding UI Content for Anonymous Users

--- KlioBlazor/Components/Account/DummyAuthenticationStateProvider.cs
+++ KlioBlazor/Components/Account/DummyAuthenticationStateProvider.cs
@@ -7,8 +7,12 @@ namespace KlioBlazor.Components.Account
     {
         public async override Task<AuthenticationState> GetAuthenticationStateAsync()
         {
-            await Task.Delay(3000);
-            var anonymous = new ClaimsIdentity();
+            // await Task.Delay(3000);
+            var anonymous = new ClaimsIdentity(new List<Claim>()
+            {
+                new Claim(ClaimTypes.Name, "Eugene"),
+                // new Claim(ClaimTypes.Role, "Admin")
+            }, "test");
             return await Task.FromResult(new AuthenticationState(new ClaimsPrincipal(anonymous)));
         }
     }
